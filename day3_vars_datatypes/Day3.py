766d97a04b32484919f836afc3796e1a23a7fff2
Pooja-47 <poojakanwar5117@gmail.com>
2026-03-25 20:01:30 +0530

    Day 3: variables and data types

---
+++ day3_vars_datatypes/Day3.py
@@ -0,0 +1,43 @@
+#Variables and Datatypes:
+  # VARIABLES:
+print()
+a=1    #1 is stored in memory and address is given to a 
+b=True
+c="Pooja"
+d=None
+print("a=",a,"\n","b=",b,"\n","c=",c,"\n","d=",d)
+
+
+#Here a,b,c,d are variables(containers) to store data in memory.
+#Assigned values of different datatypes.
+
+#DATA TYPES:
+print()
+a=35
+print(type(a),end=" ") #int
+b=True
+print(type(b),end=" ") #bool
+c="Pooja"
+print(type(c),end=" ") #str
+d=None
+print(type(d),end=" ") #NoneType
+
+# Data type defines the kind of value a variable holds
+# It may be integer, string, boolean, None
+print()
+a1="Hello23" 
+a2="99"
+print(a1+a2) # String concatination (joining strings)
+
+# We can perform operations on similar datatypes.
+# Built in datatypes
+print()
+x=45 #integers
+y=0.11 #float(2.98,99.0,-67.98)
+z=False #boolean(True, False)
+xy=[1,2,[9.8,0.89],["pooja","cs"]] # list: ordered, mutable (can be changed)
+yz=(1,2,(9.8,0.89),("pooja","cs")) # tuple: ordered, immutable (cannot be changed)
+
+print(x, y, z, xy, yz)
+
+
